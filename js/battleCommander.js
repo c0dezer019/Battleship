8dea3cb22fe9e17dc6251dc4ba3bc63b84886be3
Brian Blankenship <brian.ga.edu@gmail.com>
2020-10-09 10:59:56 -0500

    Fixed combat issue

--- js/battleCommander.js
+++ js/battleCommander.js
@@ -13,29 +13,43 @@ const game = {
     }
 }
 
-const checkCoords = (coords) => occupiedCCoords.includes(coords);
-const computerGrid = document.querySelectorAll('.comp-grid-item');
+const checkCoords = (coords) => {
+    for (let i = 0; i < occupiedCCoords.length; i++) {
+        if (occupiedCCoords[i].includes(coords)) {
+            return true;
+        }
+    }
+};
+
 //Prep the field for combat.
 function gameStart() {
     document.querySelector('.theButtonZone').remove()
     // No need for player to attack own field or select ship pieces.
     shipSelect.removeEventListener('click', handleClick);
-    //pGridCoords.removeEventListener('click', handleClick);
+    pGridCoords.removeEventListener('click', handleClick);
     game.changeStage('started');
 
     console.log(game.turn)
     if (game.getStage() == 'started') {
+        console.log('Entering started')
         if (game.getTurn() == 'player') {
+            console.log('Entering turn')
+            const computerGrid = document.querySelectorAll('.comp-grid-item');
             computerGrid.forEach(el => el.addEventListener('click', combatHandler));
 
             function combatHandler(e) {
-                if (e.target.id === checkCoords(e.target.id)) {
+                if (checkCoords(e.target.id)) {
+                    console.log(e.target.id)
                     document.getElementById(e.target.id).style.backgroundColor = 'red';
                     console.log('Hit!');
                 } else {
                     console.log("Something isn't working right.")
+                    console.log(e.target.id)
                 }
             }
-        } else("what is going on here?")
+        } else {
+            console.log("what is going on here?")
+        }
+
     }
 }
