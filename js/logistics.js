1ecf9f86de9f774af1d1607e661f674f29e63e34
c0dezer019 <c0dezer019@zohomail.com>
2021-04-08 15:17:46 -0500

    DRYing up the code a bit.

--- js/logistics.js
+++ js/logistics.js
@@ -1,34 +1,16 @@
+import { GamePiece} from "./GamePiece";
+
 // Makes the game pieces that will make up the board and assign them to the board
-const ships = {
-    carrier: {
-        size: 5,
-        coords: [],
-        rotated: false
-    },
-    battleship: {
-        size: 4,
-        coords: [],
-        rotated: false
-    },
-    cruiser: {
-        size: 3,
-        coords: [],
-        rotated: false
-    },
-    submarine: {
-        size: 3,
-        coords: [],
-        rotated: false
-    },
-    destroyer: {
-        size: 2,
-        coords: [],
-        rotated: false
-    },
-    changeProperty: function (type, attr, val) {
-        this[type][attr] = val
-    }
-}
+/* Piece sizes:
+    carrier: 5
+    battleship: 4
+    cruiser: 3
+    submarine: 3
+    destroyer: 2
+
+    *By default all game pieces will be set horizontally.
+ */
+
 
 const player = {
     "color": "rgb(79, 79, 241)"
@@ -37,63 +19,34 @@ const computer = {
     "color": "red"
 }
 
-// Create the pieces
+// Create the
+const gamePieces = [
+    new GamePiece('Carrier', 5),
+    new GamePiece('Battleship', 4),
+    new GamePiece('Cruiser', 3),
+    new GamePiece('Submarine', 3),
+    new GamePiece('Destroyer', 2),
+];
 
-const pPieces = document.getElementById('playerPieces');
-const c = document.createElement('div');
-pPieces.appendChild(c);
-c.setAttribute('id', 'carrierD');
-c.setAttribute('class', 'shipPiece');
-for (let i = 0; i < ships.carrier.size; i++) {
-    const pip = document.createElement('span');
-    pip.style.backgroundColor = player.color;
-    pip.setAttribute('class', 'healthPip')
-    pip.setAttribute('id', 'carrier');
-    c.appendChild(pip);
-}
-const bs = document.createElement('div');
-pPieces.appendChild(bs);
-bs.setAttribute('id', 'battleshipD');
-bs.setAttribute('class', 'shipPiece');
-for (let i = 0; i < ships.battleship.size; i++) {
-    const pip = document.createElement('span');
-    pip.style.backgroundColor = player.color;
-    pip.setAttribute('class', 'healthPip')
-    pip.setAttribute('id', 'battleship');
-    bs.appendChild(pip);
-}
-const cr = document.createElement('div');
-pPieces.appendChild(cr);
-cr.setAttribute('id', 'cruiserD');
-cr.setAttribute('class', 'shipPiece');
-for (let i = 0; i < ships.cruiser.size; i++) {
-    const pip = document.createElement('span');
-    pip.style.backgroundColor = player.color;
-    pip.setAttribute('class', 'healthPip')
-    pip.setAttribute('id', 'cruiser');
-    cr.appendChild(pip);
-}
-const sub = document.createElement('div');
-pPieces.appendChild(sub);
-sub.setAttribute('id', 'submarineD');
-sub.setAttribute('class', 'shipPiece');
-for (let i = 0; i < ships.submarine.size; i++) {
-    const pip = document.createElement('span');
-    pip.style.backgroundColor = player.color;
-    pip.setAttribute('class', 'healthPip')
-    pip.setAttribute('id', 'submarine');
-    sub.appendChild(pip);
-}
-const des = document.createElement('div');
-pPieces.appendChild(des);
-des.setAttribute('id', 'destroyerD');
-des.setAttribute('class', 'shipPiece');
-for (let i = 0; i < ships.destroyer.size; i++) {
-    const pip = document.createElement('span');
-    pip.style.backgroundColor = player.color;
-    pip.setAttribute('class', 'healthPip')
-    pip.setAttribute('id', 'destroyer');
-    des.appendChild(pip);
+const players = ['player', 'computer'];
+
+for (let i = 0; i < players.length; i++) {
+    const pieces = document.getElementById(`${players[i]}Pieces`);
+
+    for (let j = 0; j < gamePieces.length; j++) {
+        const piece  = document.createElement('div');
+        piece.setAttribute('id', gamePieces[i].type);
+        piece.setAttribute('class', 'gamePiece');
+        pieces.appendChild(piece);
+
+        for (let k = 0; k < gamePieces[i].pSize; k++) {
+            const pip = document.createElement('span');
+            pip.style.backgroundColor = player.color;
+            pip.setAttribute('class', 'healthPip');
+            pip.setAttribute('id', gamePieces[i].type);
+            piece.appendChild(pip)
+        }
+    }
 }
 
 const buttonZone = document.createElement('div');
@@ -109,14 +62,3 @@ const rotBtn = document.createElement('button');
 rotBtn.setAttribute('id', 'rotate');
 rotBtn.innerText = 'Rotate';
 document.querySelector('.theButtonZone').appendChild(rotBtn);
-
-const notice = document.getElementById('content');
-const modTitle = document.getElementById('modalTitle');
-
-function warning (title, msg) {
-    modTitle.innerText = title.toString();
-    notice.innerHTML = msg.toString();
-    $(document).ready(function () {
-        $("#alertModal").modal();
-    });
-}
