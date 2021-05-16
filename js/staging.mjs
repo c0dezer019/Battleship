6eee7c980af3acdac16c687cfd778743438c0881
c0dezer019 <c0dezer019@zohomail.com>
2021-05-16 15:44:19 -0500

    Repurposing file to perform all staging mode actions.

---
+++ js/staging.mjs
@@ -0,0 +1,12 @@
+import { generateAlert } from "./utilities.mjs";
+
+const occupiedPCoords = [];
+const occupiedCCoords = [];
+const playerPlacedShips = [];
+const computerPlacedShips = [];
+
+const stage = (piece, x, y) => {
+
+};
+
+export default stage;
